42ffa6d371216ab2260a700cfb0ba8f9f6f46e58
40423118 <40423118@gm.nfu.edu.tw>
2017-04-27 11:33:10 +0800

    +-*/

---
+++ w10/appcross.py
@@ -0,0 +1,5 @@
+import sys
+sys.path.append("./m1")
+import cross
+sum=cross.cross(3,20)
+print(sum)
---
+++ w10/appdiv.py
@@ -0,0 +1,5 @@
+import sys
+sys.path.append("./m1")
+import div
+sum=div.div(1,200000)
+print(sum)
